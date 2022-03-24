305bde62e585df97c8ee2c5330040beb204cc25a
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-24 21:31:33 +0530

    Day 23 Collections AddressBook

---
+++ Day23AddressBook/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23AddressBook
+{
+    public class Address
+    {
+        public string firstName, lastName, address, city, state, zip, phoneNumber, email;
+    }
+}
