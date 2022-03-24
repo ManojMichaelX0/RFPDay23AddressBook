9ba0907eb8301fecb63828304bc41df5b3676896
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-24 21:49:48 +0530

    added Uc 2 Address Book add new Deetails

--- Day23AddressBook/Address.cs
+++ Day23AddressBook/Address.cs
@@ -8,6 +8,7 @@ namespace Day23AddressBook
 {
     public class Address
     {
-        public string firstName, lastName, address, city, state, zip, phoneNumber, email;
+        public string firstName, firstName2, lastName, lastName2, address, address2, city, city2, state, state2, zip, zip2, phoneNo, phoneNo2, email, email2;
+        public int value;
     }
 }
